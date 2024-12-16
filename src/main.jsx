62ddb18e5c04bcc728be8e2e946750f2c326544b
JA <jahtiainen81@gmail.com>
2024-12-16 16:13:26 +0200

    muuttaa sovelluksen käyttämään koko ruudun korkeutta

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './components/App'
+import './assets/styles/main.scss'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
