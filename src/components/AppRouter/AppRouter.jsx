800bf411866864815196128774b584b63577c9ae
JA <jahtiainen81@gmail.com>
2025-01-13 18:07:04 +0200

    lisää AddItem- ja ItemForm-komponentit sekä reitityksen

--- src/components/AppRouter/AppRouter.jsx
+++ src/components/AppRouter/AppRouter.jsx
@@ -4,6 +4,7 @@ import Items from '../Items'
 import Root from '../Root'
 import Settings from '../Settings'
 import Stats from '../Stats'
+import AddItem from '../AddItem'
 
 function AppRouter(props) {
    const router = createBrowserRouter([
@@ -15,7 +16,8 @@ function AppRouter(props) {
             { path: "", element: <Items />, 
               loader: () => { return props.data } },
             { path: "stats", element: <Stats /> },
-            { path: "settings", element: <Settings /> }
+            { path: "settings", element: <Settings /> },
+            { path: "add", element: <AddItem /> }
          ]
       }
    ])
